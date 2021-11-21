a6eca5c51a9fc395ee277cc2100e01909841ff2e
OTHMAN\othman <othman.subhi.123@gmail.com>
2021-11-22 00:14:30 +0200

    sending and testing without update status - othman

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -4,10 +4,12 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.*;
 
 import javax.swing.JButton;
@@ -19,11 +21,16 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import com.sun.nio.sctp.SendFailedNotification;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class MainPanel extends JFrame {
 
 	private String usernameLoggedIn;
 	private List<String> Onusers = new ArrayList<>();
-
+	private String sendingTo;
 	private JTextField usernametxt;
 	private JTextField statustxt;
 	private JTextField TSItxt;
@@ -89,8 +96,8 @@ public class MainPanel extends JFrame {
 		scrollPane2.setBounds(20, 303, 430, 86);
 		getContentPane().add(scrollPane2);
 
-		JTextArea textArea2 = new JTextArea();
-		scrollPane2.setViewportView(textArea2);
+		JTextArea messageTF = new JTextArea();
+		scrollPane2.setViewportView(messageTF);
 
 		JLabel statuslbl = new JLabel("Status:");
 		statuslbl.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -189,13 +196,41 @@ public class MainPanel extends JFrame {
 		RePtxt.setColumns(10);
 
 		JButton btnNewButton = new JButton("Send");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] t = sendingTo.split(",");
+				int port = Integer.parseInt(t[1]);
+				InetAddress ip = null;
+				try {
+					ip = InetAddress.getByName(t[0]);
+					sendMsg(usernameLoggedIn + " : " + messageTF.getText() + " \n", ip, port);
+				} catch (SocketException | UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton.setBounds(460, 345, 67, 44);
+		btnNewButton.setBounds(460, 338, 100, 20);
 		getContentPane().add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("test connection");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] t = sendingTo.split(",");
+				int port = Integer.parseInt(t[1]);
+				InetAddress ip = null;
+				try {
+					ip = InetAddress.getByName(t[0]);
+					sendMsg("test connection\n", ip, port);
+				} catch (SocketException | UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton_1.setBounds(562, 357, 130, 21);
+		btnNewButton_1.setBounds(582, 315, 133, 21);
 		getContentPane().add(btnNewButton_1);
 
 		JLabel OnUlbl = new JLabel("Online Users");
@@ -213,6 +248,20 @@ public class MainPanel extends JFrame {
 		textAreaOnU.setEditable(false);
 		textAreaOnU.setFont(new Font("Courier New", Font.BOLD, 11));
 		scrollPaneOnU.setViewportView(textAreaOnU);
+
+		JButton btnNewButton_2 = new JButton("Send to all");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton_2.setBounds(460, 369, 100, 20);
+		getContentPane().add(btnNewButton_2);
+	}
+
+	protected void reprintOnlineUsers() {
+		// TODO Auto-generated method stub
+
 	}
 
 	void recievingMsg() throws Exception {
@@ -228,13 +277,27 @@ public class MainPanel extends JFrame {
 			InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
 			handleRecievedData(sentence, IPAddress, port);
-			String response = "recieved";
-			sendData = response.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			serverSocket.send(sendPacket);
-
 		}
+	}
 
+	private void sendMsg(String sentence, InetAddress ip, int port) throws SocketException {
+		byte[] sendData = new byte[2048];
+		DatagramSocket clientSocket;
+		clientSocket = new DatagramSocket();
+
+		sendData = sentence.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip, port);
+		try {
+			clientSocket.send(sendPacket);
+		} catch (IOException e) {
+			String user = ip + "," + port;
+			if (isOnlineCheck(user))
+				for (int i = 0; i < Onusers.size(); i++)
+					if (Onusers.get(i).equals(user)) {
+						Onusers.remove(i);
+						reprintOnlineUsers();
+					}
+		}
 	}
 
 	private void handleRecievedData(String sentence, InetAddress iPAddress, int port) {
@@ -257,16 +320,21 @@ public class MainPanel extends JFrame {
 		statustxt.setText(msg);
 	}
 
-	private void isOnlineCheck(String sender) {
+	private void isOnlineCheckAndAdd(String sender) {
+		boolean isfound = isOnlineCheck(sender);
+		if (isfound == false) {
+			Onusers.add(sender);
+		}
+	}
+
+	private boolean isOnlineCheck(String sender) {
 		boolean isfound = false;
 		for (String tmp : Onusers) {
 			if (tmp.equals(sender)) {
 				isfound = true;
 			}
 		}
-		if (isfound == false) {
-			Onusers.add(sender);
-		}
+		return isfound;
 	}
 
 	public static void main(String[] args) throws Exception {
